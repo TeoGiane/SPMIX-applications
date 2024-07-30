4cbb228c76ac72fa238f057a6d6e434d8bda1141
TeoGiane <gianeteo@hotmail.it>
2024-07-30 11:02:22 +0200

    Minor code changes

--- SimulatedScenarios/BD_Scenario1/R/generate_tables.R
+++ SimulatedScenarios/BD_Scenario1/R/generate_tables.R
@@ -1,8 +1,11 @@
-# Table for CM ------------------------------------------------------------
-
 # Define values for H and rho
 H <- c(2,4,6,8,10,'RJ')
 rho <- c(0,0.5,0.9,0.95,0.99)
+
+# Define subfolder
+alphabeta <- "alpha6_beta6"
+
+# Table for CM ------------------------------------------------------------
 
 # Generate CM tables
 precisions <- matrix(NA, nrow = length(H), ncol = length(rho), dimnames = list(H,rho))
@@ -11,7 +14,7 @@ specificities <- matrix(NA, nrow = length(H), ncol = length(rho), dimnames = lis
 for (i in 1:length(H)) {
   for (j in 1:length(rho)) {
     # Parse csv file
-    file_name <- file.path(getwd(), "summary", sprintf("CM-H_%s-rho_%s.csv", H[i], rho[j]))
+    file_name <- file.path(getwd(), "summary", alphabeta, sprintf("CM-H_%s-rho_%s.csv", H[i], rho[j]))
     CM <- read.csv(file_name)
     # Compute precision
     prec_vec <- sapply(1:nrow(CM), function(e){CM[e,"TP"]/(CM[e,"TP"]+CM[e,"FP"])}); prec_vec[which(is.nan(prec_vec))] <- 0
@@ -27,16 +30,12 @@ for (i in 1:length(H)) {
 
 # Table for meanL1 --------------------------------------------------------
 
-# Define values for H and rho
-H <- c(2,4,6,8,10,'RJ')
-rho <- c(0,0.5,0.9,0.95,0.99)
-
 # Generate CM tables
 meanL1 <- matrix(NA, nrow = length(H), ncol = length(rho), dimnames = list(H,rho))
 for (i in 1:length(H)) {
   for (j in 1:length(rho)) {
     # Parse csv file
-    file_name <- file.path(getwd(), "summary", sprintf("meanL1-H_%s-rho_%s.csv", H[i], rho[j]))
+    file_name <- file.path(getwd(), "summary", alphabeta, sprintf("meanL1-H_%s-rho_%s.csv", H[i], rho[j]))
     L1_df <- read.csv(file_name)
     # Compute overall mean L1 distance
     meanL1[i,j] <- sprintf("%g (%g)", round(mean(L1_df[,"MeanL1"]),3), round(sd(L1_df[,"MeanL1"]), 3))
@@ -55,9 +54,9 @@ meanWAIC <- matrix(NA, nrow = length(H), ncol = length(rho), dimnames = list(H,r
 for (i in 1:length(H)) {
   for (j in 1:length(rho)) {
     # Parse csv file
-    file_name <- file.path(getwd(), "summary", sprintf("meanWAIC-H_%s-rho_%s.csv", H[i], rho[j]))
+    file_name <- file.path(getwd(), "summary", alphabeta, sprintf("meanWAIC-H_%s-rho_%s.csv", H[i], rho[j]))
     WAIC_df <- read.csv(file_name)
     # Compute overall mean L1 distance
-    meanWAIC[i,j] <- sprintf("%g (%g)", round(mean(L1_df[,"MeanWAIC"]),3), round(sd(L1_df[,"MeanWAIC"]), 3))
+    meanWAIC[i,j] <- sprintf("%g (%g)", round(mean(WAIC_df[,"WAIC"]),1), round(sd(WAIC_df[,"WAIC"]), 1))
   }
 }
