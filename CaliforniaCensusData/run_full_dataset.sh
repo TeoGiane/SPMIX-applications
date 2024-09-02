ea0f3a212c5a4c5a531769e7c1c29ab27ec3077c
TeoGiane <gianeteo@hotmail.it>
2024-09-02 16:56:05 +0200

    Store full dataset + full dataset sim study

---
+++ CaliforniaCensusData/run_full_dataset.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set values for H and RHO
+H=(RJ)
+RHO=(0 0.9 0.95 0.99)
+
+# Make log folders if not present
+for h in ${H[@]}; do
+  for r in ${RHO[@]}; do
+    mkdir -p log/H_$h/rho_$r
+  done
+done
+
+# Execute run_sampler.R in parallel via GNU parallel
+parallel -j 15 \
+  '; Rscript --vanilla ./R/run_sampler.R -c {1} -r {2} -o ./output/H_{1}/rho_{2}/chain_$n.dat ./data/full_dataset.dat &> ./log/H_{1}/rho_{2}/run_full_dataset.log' \
+  ::: ${H[@]} ::: ${RHO[@]}
+
+# Finished job notification
+echo "JOB 'run_full_dataset.sh' has finished" | mail -s "[Notification] - Job finished" matteo.gianella@polimi.it
--- CaliforniaCensusData/run_sim_study.sh
+++ CaliforniaCensusData/run_sim_study.sh
@@ -9,12 +9,12 @@ RHO=(0 0.9 0.95 0.99)
 
 # Make log folders if not present
 for h in ${H[@]}; do
-    for r in ${RHO[@]}; do
-        mkdir -p log/H_$h/rho_$r
-    done
+  for r in ${RHO[@]}; do
+    mkdir -p log/H_$h/rho_$r
+  done
 done
 
 # Execute run_sampler.R in parallel via GNU parallel
 parallel -j 15 \
-    'printf -v n "%03d" {1}; Rscript --vanilla ./R/run_sampler.R -c {2} -r {3} -o ./output/H_{2}/rho_{3}/chain_$n.dat ./data/data_$n.dat &> ./log/H_{2}/rho_{3}/run_sampler_$n.log' \
-    ::: $(eval echo {1..$NSIM}) ::: ${H[@]} ::: ${RHO[@]}
+  'printf -v n "%03d" {1}; Rscript --vanilla ./R/run_sampler.R -c {2} -r {3} -o ./output/H_{2}/rho_{3}/chain_$n.dat ./data/data_$n.dat &> ./log/H_{2}/rho_{3}/run_sampler_$n.log' \
+  ::: $(eval echo {1..$NSIM}) ::: ${H[@]} ::: ${RHO[@]}
