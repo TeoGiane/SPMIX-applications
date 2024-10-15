35e788cce3c3612bccd8ddc305fa64b6f6c6e2f8
TeoGiane <gianeteo@hotmail.it>
2024-10-15 08:46:17 +0200

    Code fix

--- JAGSModels/run_sampler.sh
+++ JAGSModels/run_sampler.sh
@@ -4,7 +4,7 @@
 mkdir -p log/JAGSModel
 
 # Execute run_sampler.R in parallel via GNU parallel
-Rscript --vanilla ./R/new_multivariate_CAR.R &> ./log/JAGSModel/newnew_multivariate_CAR.log
+Rscript --vanilla ./R/newnew_multivariate_CAR.R &> ./log/JAGSModel/newnew_multivariate_CAR.log
 
 # Finished job notification
 echo "JOB 'run_sampler.sh' has finished" | mail -s "[Notification] - Job finished" matteo.gianella@polimi.it
