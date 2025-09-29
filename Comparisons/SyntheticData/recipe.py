d82aa1a422a4fb50930e6efc9b3bdd1141bd359f
Matteo Gianella <gianeteo@hotmail.it>
2025-09-29 23:01:53 +0200

    Update recipe.py

--- Comparisons/SyntheticData/recipe.py
+++ Comparisons/SyntheticData/recipe.py
@@ -16,4 +16,12 @@ create_task("pip-sync", action="pip-sync", dependencies=["requirements.txt"])
 # Create generate-data task
 action = ["Rscript","--vanilla","src/generate_data.R"]
 deps = ["src/generate_data.R"]
-create_task(name="generate_data", dependencies=deps, action=action)
+input_targets = ["input/data.dat", "input/shp/grid.dbf", "input/shp/grid.prj","input/shp/grid.shp", "input/shp/grid.shx"]
+create_task(name="generate_data", dependencies=deps, action=action, targets=input_targets)
+
+# Create tasks for all algorithms
+MODELS = ["SPMIX", "CARBayes", "naiveMCAR", "SKATER"]
+for model in MODELS:
+    action = ["Rscript","--vanilla",f"src/run_{model}.R"]
+    deps = [f"src/run_{model}.R"] + input_targets
+    create_task(name=f"run_{model}", dependencies=deps, action=action)
