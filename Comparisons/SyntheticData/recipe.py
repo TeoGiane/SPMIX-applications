6ed5a32d7d8f2de1df9c6c0d20c7c57b7e557cba
Matteo Gianella <gianeteo@hotmail.it>
2025-10-01 17:51:24 +0200

    Removed un-necessary tasks

--- Comparisons/SyntheticData/recipe.py
+++ Comparisons/SyntheticData/recipe.py
@@ -1,17 +1,12 @@
-from cook import create_task # , Task
+from cook import create_task
+
+# , Task
 # from cook.contexts import create_group
 # import itertools as it
 # import numpy as np
 # import os
 # from pathlib import Path
 # from typing import Literal
-
-# Create requirements task
-create_task("requirements", action="pip-compile -v", targets=["requirements.txt"],
-            dependencies=["requirements.in"])
-
-# Create pip-sync task
-create_task("pip-sync", action="pip-sync", dependencies=["requirements.txt"])
 
 # Create generate-data task
 action = ["Rscript","--vanilla","src/generate_data.R"]
@@ -22,6 +17,6 @@ create_task(name="generate_data", dependencies=deps, action=action, targets=inpu
 # Create tasks for all algorithms
 MODELS = ["SPMIX", "CARBayes", "naiveMCAR", "SKATER"]
 for model in MODELS:
-    action = ["Rscript","--vanilla",f"src/run_{model}.R"]
+    action = ["Rscript", "--vanilla", f"src/run_{model}.R"]
     deps = [f"src/run_{model}.R"] + input_targets
     create_task(name=f"run_{model}", dependencies=deps, action=action)
