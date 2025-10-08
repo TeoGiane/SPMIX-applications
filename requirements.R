eaaffdb0c1c3ec3f0854f732f6085004d28b3614
Matteo Gianella <gianeteo@hotmail.it>
2025-10-08 18:26:15 +0200

    Add new dependencies

--- requirements.R
+++ requirements.R
@@ -2,14 +2,27 @@
 repos <- "https://cloud.r-project.org/"
 
 # List of required packages
-package_deps <- c("devtools", "sf", "spdep", "CARBayes", "argparser", "rjags", "reshape2", "RProtoBuf")
+package_deps <- c("argparser",
+                  "CARBayes",
+                  "devtools",
+                  "dplyr",
+                  "ggplot2",
+                  "ggmap",
+                  "ggrepel",
+                  "ggridges",
+                  "gridExtra",
+                  "reshape2",
+                  "rjags",
+                  "RProtoBuf",
+                  "sf",
+                  "spdep")
 
 # Install packages from CRAN
 install.packages(package_deps, repos = repos)
-# install.packages("CARBayes", repos = repos)
-# install.packages("argparser", repos = repos, dependencies = FALSE)
-# install.packages("rjags", repos = repos, dependencies = FALSE)
-# install.packages("RProtoBuf", repos = repos, dependencies = FALSE)
+
+# Register stadia map API key for ggmap
+ggmap::register_stadiamaps(key = "1f3e613a-2adc-4c22-8bc9-901f1c33e05b",
+                           write = TRUE)
 
 # Install SPMIX from GitHub
 devtools::install_github("TeoGiane/SPMIX")
