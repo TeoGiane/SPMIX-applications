0d9e1df76bfd5ad168c010ef442e0b71dea072b1
Matteo Gianella <gianeteo@hotmail.it>
2025-10-09 15:31:10 +0200

    Minor code changes

--- Comparisons/SyntheticData/recipe.py
+++ Comparisons/SyntheticData/recipe.py
@@ -1,29 +1,24 @@
 from cook import create_task
 
-# , Task
-# from cook.contexts import create_group
-# import itertools as it
-# import numpy as np
-# import os
-# from pathlib import Path
-# from typing import Literal
-
 # Create generate-data task
-generate_data_action = ["Rscript","--vanilla","src/generate_data.R"]
 generate_data_deps = ["src/generate_data.R"]
 generate_data_targets = ["input/data.dat", "input/shp/grid.dbf", "input/shp/grid.prj","input/shp/grid.shp", "input/shp/grid.shx"]
-create_task(name="generate_data", dependencies=generate_data_deps, action=generate_data_action, targets=generate_data_targets)
+create_task(name="generate_data", action=["Rscript","src/generate_data.R"],
+            dependencies=generate_data_deps,
+            targets=generate_data_targets)
 
 # Create tasks for all algorithms
 MODELS = ["SPMIX", "CARBayes", "naiveMCAR", "SKATER"]
 run_model_targets = [f"output/{model}-fit.dat" for model in MODELS]
 for model in MODELS:
-    run_model_action = ["Rscript", "--vanilla", f"src/run_{model}.R"]
     run_model_deps = [f"src/run_{model}.R"] + generate_data_targets
-    create_task(name=f"run_{model}", dependencies=run_model_deps, action=run_model_action, targets=[run_model_targets[MODELS.index(model)]])
+    create_task(name=f"run_{model}", action=["Rscript", f"src/run_{model}.R"],
+                dependencies=run_model_deps,
+                targets=[run_model_targets[MODELS.index(model)]])
 
 # Create generate_plot task
-generate_plot_action = ["Rscript","--vanilla","src/generate_plot.R"]
 generate_plot_deps = ["src/generate_plot.R"] + run_model_targets
 generate_plot_targets = [f"plots/plt_boundaries-{model}.pdf" for model in MODELS]
-create_task(name="generate_plot", dependencies=generate_plot_deps, action=generate_plot_action, targets=generate_plot_targets)
+create_task(name="generate_plot", action=["Rscript","src/generate_plot.R"],
+            dependencies=generate_plot_deps,
+            targets=generate_plot_targets)
