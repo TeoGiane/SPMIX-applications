d29098e602c59a8131f1a8499ce805649445439e
TeoGiane <gianeteo@hotmail.it>
2025-10-27 17:26:50 +0100

    New PBS spcript added

--- CaliforniaCensusData/submit_run_full_dataset_fixed_p.sh
+++ CaliforniaCensusData/submit_run_full_dataset_fixed_p.sh
@@ -3,13 +3,17 @@
 # PBS Settings
 #PBS -S /bin/bash
 #PBS -l select=1:ncpus=24:mem=64gb
-#PBS -l walltime=144:00:00
-#PBS -N SPMIX-CaliforniaCensusData-run_full_dataset
-#PBS -o log/SPMIX-CaliforniaCensusData-run_full_dataset.out
-#PBS -e log/SPMIX-CaliforniaCensusData-run_full_dataset.err
+#PBS -l walltime=240:00:00
+#PBS -N SPMIX-CaliforniaCensusData-run_full_dataset_fixed_p
+#PBS -o log/SPMIX-CaliforniaCensusData-run_full_dataset_fixed_p.out
+#PBS -e log/SPMIX-CaliforniaCensusData-run_full_dataset_fixed_p.err
 
 # Set current working directory
 cd ${PBS_O_WORKDIR}
+
+# Load required modules
+source /opt/mox/spack/share/spack/setup-env.sh
+spack load parallel
 
 # Create alias for apptainer execution
 export APPTAINER=/opt/mox/apptainer/bin/apptainer
@@ -18,5 +22,15 @@ in-apptainer () {
 }
 export -f in-apptainer
 
-# Execution in containerized environment
-in-apptainer cook exec run_full_dataset &> run_full_dataset.log
+# Define graph_sparsity values
+SPARSITIES=(0.1 0.2 0.3 0.4 0.5)
+
+# Generate log directories
+for SPARSITY in "${SPARSITIES[@]}"; do
+	mkdir -p log/HRJ/rho0.95/alpha24_beta22/p_${SPARSITY}
+done
+
+# Parallel execution in containerized environment
+parallel -j 0 'in-apptainer cook exec run_full_dataset_fixed_p_{1} &> log/HRJ/rho0.95/alpha24_beta22/p_{1}/run_full_dataset_fixed_p.log' ::: "${SPARSITIES[@]}"
+
+
