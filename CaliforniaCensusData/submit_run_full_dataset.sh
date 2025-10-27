ab5f2c0f39cdf55c2ea62da1e1025c92db6f2909
TeoGiane <gianeteo@hotmail.it>
2025-10-27 16:20:46 +0100

    Improved log in job submission

--- CaliforniaCensusData/submit_run_full_dataset.sh
+++ CaliforniaCensusData/submit_run_full_dataset.sh
@@ -3,13 +3,16 @@
 # PBS Settings
 #PBS -S /bin/bash
 #PBS -l select=1:ncpus=24:mem=64gb
-#PBS -l walltime=144:00:00
+#PBS -l walltime=240:00:00
 #PBS -N SPMIX-CaliforniaCensusData-run_full_dataset
 #PBS -o log/SPMIX-CaliforniaCensusData-run_full_dataset.out
 #PBS -e log/SPMIX-CaliforniaCensusData-run_full_dataset.err
 
 # Set current working directory
 cd ${PBS_O_WORKDIR}
+
+# Create log directory
+mkdir -p log/HRJ/rho0.95/alpha24_beta22/a2_b93/
 
 # Create alias for apptainer execution
 export APPTAINER=/opt/mox/apptainer/bin/apptainer
@@ -19,4 +22,4 @@ in-apptainer () {
 export -f in-apptainer
 
 # Execution in containerized environment
-in-apptainer cook exec run_full_dataset &> run_full_dataset.log
+in-apptainer cook exec run_full_dataset &> log/HRJ/rho0.95/alpha24_beta22/a2_b93/run_full_dataset.log
