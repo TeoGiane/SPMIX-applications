a5537948db518f6c7ae8c617fceb60648decbfc3
TeoGiane <gianeteo@hotmail.it>
2025-10-28 18:00:44 +0100

    Create new submission script

---
+++ BoundaryDetection/BD_Scenario1/submit_compute_confusion_matrices.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# PBS Settings
+#PBS -S /bin/bash
+#PBS -l select=1:ncpus=50:mem=128gb
+#PBS -l walltime=48:00:00
+#PBS -N BD_Scenario1-compute_confusion_matrices
+#PBS -o /u/gianella/log/BD_Scenario1-compute_confusion_matrices.out
+#PBS -e /u/gianella/log/BD_Scenario1-compute_confusion_matrices.err
+
+# Set current working directory
+cd ${PBS_O_WORKDIR}
+
+# Create alias for apptainer execution
+export APPTAINER=/opt/mox/apptainer/bin/apptainer
+export SPMIX=/u/gianella/containers/spmix.sif
+in-apptainer () {
+	$APPTAINER exec --pwd /workdir --bind `pwd`:/workdir $SPMIX $@
+}
+export -f in-apptainer
+
+# Create log directory if it doesn't exists
+mkdir -p log
+
+# Execution in containerized environment
+in-apptainer cook exec --jobs 50 simulation_study:compute_confusion_matrices &> log/compute_confusion_matrices.log
