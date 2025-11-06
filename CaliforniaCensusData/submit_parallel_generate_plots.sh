0d47b3474ce6d68e2e431a676ac9d204c266290e
TeoGiane <gianeteo@hotmail.it>
2025-11-06 10:18:28 +0100

    New submission script

---
+++ CaliforniaCensusData/submit_parallel_generate_plots.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# PBS Settings
+#PBS -S /bin/bash
+#PBS -l select=1:ncpus=24:mem=64gb
+#PBS -l walltime=240:00:00
+#PBS -N SPMIX-CaliforniaCensusData-parallel_generate_plots
+#PBS -o log/SPMIX-CaliforniaCensusData-parallel_generate_plots.out
+#PBS -e log/SPMIX-CaliforniaCensusData-parallel_generate_plots.err
+
+# Set current working directory
+cd ${PBS_O_WORKDIR}
+
+# Create log directory
+mkdir -p log
+
+# Create alias for apptainer execution
+export APPTAINER=/opt/mox/apptainer/bin/apptainer
+export SPMIX=/u/gianella/containers/spmix_dev.sif
+in-apptainer () {
+	$APPTAINER exec --pwd /workdir --bind `pwd`:/workdir $SPMIX $@
+}
+export -f in-apptainer
+
+# Execution in containerized environment
+in-apptainer cook exec --jobs 2 parallel_generate_plots &> log/parallel_generate_plots.log
+
